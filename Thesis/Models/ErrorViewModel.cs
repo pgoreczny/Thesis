94e9ce5376d171846a961838220a752fd4372898
Piotr Góreczny <martinblake1996@gmail.com>
2023-02-01 16:53:46 +0100

    Rehash

---
+++ Thesis/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Thesis.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
