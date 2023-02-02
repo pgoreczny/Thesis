8d2385f8a5a3b3664afcac7d4a9d5597d2e66fb1
Piotr Góreczny <martinblake1996@gmail.com>
2023-02-02 15:27:12 +0100

    WYSIWYG editor

---
+++ Thesis/Controllers/TextController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Thesis.Controllers
+{
+    public class TextController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult TextEditor()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public int save([FromBody] string content)
+        {
+            return 0;
+        }
+    }
+}
