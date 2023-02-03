9fde8703182d06c4dcc46a027ab5b269b6624ef4
Piotr Góreczny <martinblake1996@gmail.com>
2023-02-03 10:45:12 +0100

    File management

---
+++ Thesis/Controllers/FileController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Thesis.Controllers
+{
+    public class FileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult save(IFormFile file)
+        {
+            if (file != null)
+            {
+                //TODO
+                //get list of allowed extensions from db
+                List<string> extensions = new List<string>();
+                extensions.Add(".pdf");
+                extensions.Add(".docx");
+                extensions.Add(".jpg");
+                if (extensions.Contains(Path.GetExtension(file.FileName)) && file.Length < 4194304)
+                {
+                    string ImageName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string SavePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img", ImageName);
+
+                    using (var stream = new FileStream(SavePath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                }
+            }
+            return View("index");
+        }
+    }
+}
