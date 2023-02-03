49819c331150aa998891f38bad02f5a605a721d9
Piotr Góreczny <martinblake1996@gmail.com>
2023-02-03 16:35:31 +0100

    Add basic version of menu

---
+++ Thesis/Models/MenuItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Thesis.Models
+{
+    public class MenuItem
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string url { get; set; }
+        public List<MenuItem>children = new List<MenuItem>();
+        public int parent { get; set; }
+    }
+}
---
+++ Thesis/ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Thesis.Models;
+
+namespace Thesis.ViewComponents
+{
+    public class MenuViewComponent: ViewComponent
+    {
+        List<MenuItem>menu = new List<MenuItem>();
+        public MenuViewComponent()
+        {
+            MenuItem child = new MenuItem { name = "Submenu test", url = "/Home/Privacy" };
+            menu.Add(new MenuItem { Id = 1, name = "Test 1", url = "/Text/TextEditor" });
+            menu.Add(new MenuItem { Id = 2, name = "Test 2", url = "/File/Index" });
+            menu.Add(new MenuItem { Id = 3, name = "Test 3", url = "/" });
+            menu.Add(new MenuItem { Id = 4, name = "Test 4", url = "/" });
+            menu[3].children.Add(child);
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = menu;
+            return await Task.FromResult((IViewComponentResult)View("Menu", model));
+        }
+    }
+}
